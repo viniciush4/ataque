64b75343fec9ea56c78ab007e7f3edfc20d97a27
root <root@Leo>
2018-04-25 15:52:01 -0300

    O Cliente foi finalizado mas é preciso realizar testes assim que o mestre for criado.

--- src/br/inf/ufes/ppd/Cliente.java
+++ src/br/inf/ufes/ppd/Cliente.java
@@ -1,20 +1,93 @@
 package br.inf.ufes.ppd;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.Random;
 
 public class Cliente 
 {
+	private static byte[] lerArquivoCriptografado(String nomeArquivo, int tamanhoVetorGerado) throws IOException
+	{
+		File arquivo = new File(nomeArquivo);
+		byte[] mensagem;
+		
+		//Verifica se o arquivo não existe
+		if(!arquivo.exists())
+		{
+			//Cria um vetor de bytes aleatório
+	        mensagem = new byte[tamanhoVetorGerado];
+	        new Random().nextBytes(mensagem);
+	        //SecureRandom.getInstanceStrong().nextBytes(mensagem);
+	        
+	        //Salva o vetor em um arquivo
+			salvarArquivo(nomeArquivo, mensagem);
+		}
+		else
+		{	
+		    InputStream is = new FileInputStream(arquivo);
+	        long length = arquivo.length();
+	        
+	        // creates array (assumes file length<Integer.MAX_VALUE)
+	        mensagem = new byte[(int)length];
+	        
+	        int offset = 0; 
+	        int count = 0;
+	        
+	        while ((offset < mensagem.length) && (count = is.read(mensagem, offset, mensagem.length-offset)) >= 0) 
+	        {
+	            offset += count;
+	        }
+	        
+	        is.close();
+		}
+        
+        return mensagem;
+	}
+
+	private static void salvarArquivo(String nomeArquivo, byte[] mensagem) throws IOException
+	{
+		FileOutputStream out = new FileOutputStream(nomeArquivo);
+	    out.write(mensagem);
+	    out.close();
+	}
+	
 	public static void main(String[] args) 
 	{
 		Random numeroAleatorio = new Random();
 		
+		//Captura os parâmetros
 		String nomeArquivoCriptografado = (args.length < 1) ? null : args[0];
-		String palavraConhecida = (args.length < 2) ? null : args[1];
-		int tamanhoVetorGerado = (args.length < 3) ?  (1000 + numeroAleatorio.nextInt(99000)) : Integer.parseInt(args[2]);
+		byte[] palavraConhecida = (args.length < 2) ? null : args[1].getBytes();
+		int tamanhoVetorGerado = (args.length < 3) ? (1000 + numeroAleatorio.nextInt(99000)) : Integer.parseInt(args[2]);
 		
-
-
-		System.out.println(tamanhoVetorGerado);
+		try
+		{
+			//Armazena o vetor de bytes
+			byte[] mensagem = lerArquivoCriptografado(nomeArquivoCriptografado,tamanhoVetorGerado);
+			
+			//Invoca o mestre passando o vetor de bytes
+			Registry registry = LocateRegistry.getRegistry();
+			//Attacker ataque = (Attacker) registry.lookup("mestre");
+			Master mestre = (Master) registry.lookup("mestre");
+			
+			Guess[] guess = mestre.attack(mensagem, palavraConhecida);
+			
+			//Imprime as chaves candidatas e salva as mensagens candidatas em arquivos
+			for(Guess g: guess)
+			{
+				System.out.println("Chave candidata encontrada: " + g.getKey());
+				salvarArquivo(g.getKey()+".msg", g.getMessage());
+			}
+			
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
 	}
-
 }
